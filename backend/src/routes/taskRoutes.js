8cea34f433b6292f111befef705db08a718cba24
kvk1999 <koushikraghavan@gmail.com>
2025-09-02 18:01:01 +0530

    Update taskRoutes.js

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import {
-  getTasks,
-  getTaskById,
-  createTask,
-  updateTask,
-  deleteTask
-} from '../controllers/taskController.js';
+import { getTasks, createTask, updateTask, deleteTask } from '../controllers/taskController.js';
 
 const router = express.Router();
 
+router.get('/:id', getTaskById);  // <-- make sure getTaskById exists
 router.get('/', getTasks);
-router.get('/:id', getTaskById);
 router.post('/', createTask);
 router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
 
 export default router;
+
